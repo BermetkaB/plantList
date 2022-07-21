786b5d5ac46e4458863781cef515050e1c91f0e9
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-22 00:00:21 +0600

    recycle in kotlin

---
+++ app/src/main/java/com/example/plantshagbook/PlantAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.plantshagbook
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plantshagbook.databinding.PlantItemLayoutBinding
+
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
+    private var planList= emptyList<PlantModel>()
+
+    class PlantViewHolder(item: View):RecyclerView.ViewHolder(item){
+        val binding=PlantItemLayoutBinding.bind(item)
+        fun bind(plant: PlantModel)= with(binding){
+            imgPlant.setImageResource(plant.imageId)
+            tvTitle.text=plant.title
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantViewHolder {
+
+val view=LayoutInflater.from(parent.context).inflate(R.layout.plant_item_layout,parent,false)
+        return PlantViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
+
+ holder.bind(planList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+           return planList.size   }
+
+   // fun addPlant(plant: PlantModel){
+    //    planList.add(plant)
+    //    notifyDataSetChanged()
+   fun setData( newList:List<PlantModel>){
+       this.planList = newList
+       notifyDataSetChanged()
+}}
---
+++ app/src/main/java/com/example/plantshagbook/PlantModel.kt
@@ -0,0 +1,4 @@
+package com.example.plantshagbook
+
+class PlantModel(val imageId:Int, val title:String) {
+}
