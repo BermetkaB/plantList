cf15e09c931969742b561f2895134719351e4207
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-22 00:56:11 +0600

    adapter

--- app/src/main/java/com/example/plantshagbook/MainActivity.kt
+++ app/src/main/java/com/example/plantshagbook/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.plantshagbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.plantshagbook.databinding.ActivityMainBinding
+import com.example.plantshagbook.databinding.PlantItemLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter=PlantAdapter()
+    private  val imageIdList=listOf(
+            R.drawable.img,
+            R.drawable.img_1,
+        R.drawable.img_2,
+        R.drawable.img_3,
+        R.drawable.img_4)
+       private var index=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init(){
+        binding.apply {
+            recycle.layoutManager= GridLayoutManager(this@MainActivity,3)
+            recycle.adapter=adapter
+            btnAdd.setOnClickListener {
+                if (index>4) index=0
+               val plant=PlantModel(imageIdList[index],"PLANT $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
     }
 }
